61ce2c1f7241464f8bafdec4b152fe74b875a5f5
Matheus Musashi <126472450+MatheusMusashiTanaka@users.noreply.github.com>
2023-04-12 09:11:27 -0300

    Update funçoes_2.go

--- funçoes_2.go
+++ funçoes_2.go
@@ -5,7 +5,7 @@ func QuantasVogais(x string) int {
 		Frequencia_vogais[letra] += 1
 	}
 	for letra, count := range Frequencia_vogais {
-		if letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' {
+		if letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u' || letra == 'A' || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U'{
 			quantidade_vogais += count
 		}
 	}
